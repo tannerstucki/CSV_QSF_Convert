b6ad57f95ff70e21d965aa00648ddff3a2be641f
Tanner Stucki <tanner.stucki@yahoo.com>
2021-07-30 10:33:09 -0600

    Adding term and skip logic

--- conversion.py
+++ conversion.py
@@ -9,6 +9,9 @@ survey_id = "SV_" + ''.join(random.choice(string.ascii_uppercase + string.digits
 trash_block_id = "BL_" + ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(15))
 random_user_id = "UR_" + ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(15))
 response_set_id = "RS_" + ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(15))
+scoring_id = "SC_" + ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(15))
+preview_term_id = "BL_" + ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(15))
+preview_variables_id = "BL_" + ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(15))
 comma_new_line = ""
 
 #Block variables
@@ -16,6 +19,8 @@ cur_block_index = 0
 cur_block_name = ""
 cur_block_id = ""
 cur_flow_index = 3
+skip_logic_id = 1
+final_question_id = 0
 
 #The rows of the csv componenents
 question_number_index = 0
@@ -26,13 +31,14 @@ matrix_scale_points_index = 5
 display_logic_index = 7
 randomization_index = 6
 block_index = 1
+term_index = 8
 
 #Line arguments
 comma_new_line_index = 3
 
 survey_entry = {
   "SurveyID": survey_id,
-  "SurveyName": "QSF Default Upload",
+  "SurveyName": "Qualtrics Survey | Qualtrics Experience Management",
   "SurveyDescription": None,
   "SurveyOwnerID": random_user_id,
   "SurveyBrandID": "qxm",
@@ -152,7 +158,7 @@ survey_options = {
     "ValidationMessage": "",
     "PreviousButton": " \u2190 ",
     "NextButton": " \u2192 ",
-    "SurveyTitle": "QSF Default Upload",
+    "SurveyTitle": "Qualtrics Survey | Qualtrics Experience Management",
     "SkinLibrary": "qxm",
     "SkinType": "MQ",
     "Skin": "minimal-wd1",
@@ -168,6 +174,122 @@ survey_options = {
     "SurveyMetaDescription": "",
   }
 }
+
+scoring = {
+  "SurveyID": survey_id,
+  "Element": "SCO",
+  "PrimaryAttribute": "Scoring",
+  "SecondaryAttribute": None,
+  "TertiaryAttribute": None,
+  "Payload":
+  {
+      "ScoringCategories":
+      [
+          {
+              "ID": scoring_id,
+              "Name": "Score",
+              "Description": ""
+          }
+      ],
+      "ScoringCategoryGroups":
+      [],
+      "DefaultScoringCategory": scoring_id,
+      "ScoringSummaryCategory": None,
+      "ScoringSummaryAfterQuestions": 0,
+      "ScoringSummaryAfterSurvey": 0,
+      "AutoScoringCategory": None
+  }
+}
+
+preview_term_question = {
+  "SurveyID": survey_id,
+  "Element": "SQ",
+  "PrimaryAttribute": None,
+  "SecondaryAttribute": "Only shown in preview   term: ${e://Field/term} gc: ${e://Field/gc}",
+  "TertiaryAttribute": None,
+  "Payload":
+  {
+    "QuestionText": "<strong><span style=\"color:#e74c3c;\">Only shown in preview</span></strong>\n<div>&nbsp;</div>\n\n<div>term:&nbsp;${e://Field/term}</div>\n\n<div>gc:&nbsp;${e://Field/gc}</div>",
+    "DefaultChoices": False,
+    "DataExportTag": "Preivew: Term",
+    "QuestionID": None,
+    "QuestionType": "DB",
+    "Selector": "TB",
+    "Configuration":
+    {
+        "QuestionDescriptionOption": "UseText"
+    },
+    "QuestionDescription": "Only shown in preview   term: ${e://Field/term} gc: ${e://Field/gc}",
+    "ChoiceOrder":
+    [],
+    "Validation":
+    {
+      "Settings":
+      {
+          "Type": "None"
+      }
+    },
+    "GradingData":
+    [],
+    "Language":
+    [],
+    "NextChoiceId": 4,
+    "NextAnswerId": 1
+  }
+}
+
+preview_variables_question = {
+  "SurveyID": survey_id,
+  "Element": "SQ",
+  "PrimaryAttribute": None,
+  "SecondaryAttribute": "Only shown in preview   Variable Name: ",
+  "TertiaryAttribute": None,
+  "Payload":
+  {
+    "QuestionText": "<strong><span style=\"color:#e74c3c;\">Only shown in preview</span></strong>\n<div>&nbsp;</div>\n\n<div>Variable Name:&nbsp;</div>",
+    "DefaultChoices": False,
+    "DataExportTag": "Preview: Variables",
+    "QuestionID": None,
+    "QuestionType": "DB",
+    "Selector": "TB",
+    "Configuration":
+    {
+        "QuestionDescriptionOption": "UseText"
+    },
+    "QuestionDescription": "Only shown in preview   Variable Name: ",
+    "ChoiceOrder":
+    [],
+    "Validation":
+    {
+      "Settings":
+      {
+          "Type": "None"
+      }
+    },
+    "GradingData":
+    [],
+    "Language":
+    [],
+    "NextChoiceId": 4,
+    "NextAnswerId": 1
+  }
+}
+
+def resetPreviewBlock():
+  preview_block = {
+    "Type": "Standard",
+    "SubType": "",
+    "Description": None,
+    "ID": None,
+    "BlockElements":
+    [
+      {
+        "Type": "Question",
+        "QuestionID": None
+      }
+    ]
+  }
+  return preview_block
 
 def resetQuestionMultipleChoice():
   question_multiple_choice = {
@@ -274,8 +396,18 @@ def resetDisplayLogic():
       "LeftOperand": None,
       "Type": "Expression",
   }
-
   return display_logic
+
+def resetSkipLogic():
+  skip_logic = {
+    "ChoiceLocator": None,
+    "Condition": None,
+    "Locator": None,
+    "QuestionID": None,
+    "SkipLogicID": 0,
+    "SkipToDestination": None,
+  }
+  return skip_logic
 
 
 def buildDefaultQsf(qsf_output, opp_name):
@@ -284,8 +416,8 @@ def buildDefaultQsf(qsf_output, opp_name):
   qsf_output['SurveyElements'].append(survey_blocks)
   qsf_output['SurveyElements'].append(survey_flow)
   qsf_output['SurveyElements'].append(survey_options)
+  qsf_output['SurveyElements'].append(scoring)
   if opp_name != "":
-    print(opp_name)
     qsf_output['SurveyElements'][1]["Payload"]["Flow"][0]["EmbeddedData"][0]["Value"] = opp_name
   return qsf_output
 
@@ -352,7 +484,7 @@ def buildMatrixTableQuestionType(question, cur_question):
 def buildChoices(question, cur_question):
   if "matrix table" in question[question_type_index].lower() or "multiple choice" in question[question_type_index].lower():
     if comma_new_line == "comma":
-      choices = question[choices_index].split(",")
+      choices = question[choices_index].split(";")
     else:
       choices = question[choices_index].split("\n")
 
@@ -383,6 +515,7 @@ def buildChoices(question, cur_question):
       #Get anchor options
       if "Randomization" in cur_question["Payload"]:
         if anchor_option in choice.lower():
+          cur_choice["Display"] = choice.split("[", 1)[0].strip()
           if cur_question["Payload"]["Randomization"]["Type"] != "Advanced":
             #Change randomization to advanced
             FixedOrder = []
@@ -411,7 +544,7 @@ def buildChoices(question, cur_question):
 def buildMatrixTableScalePoints(question, cur_question):
   if "matrix table" in question[question_type_index].lower():
     if comma_new_line == "comma":
-      scale_points = question[matrix_scale_points_index].split(",")
+      scale_points = question[matrix_scale_points_index].split(";")
     else:
       scale_points = question[matrix_scale_points_index].split("\n")
 
@@ -494,8 +627,127 @@ def addQuestionToBlock(question_iteration, qsf_output, question, qsf_input):
     qsf_output['SurveyElements'][0]["Payload"][cur_block_index]["BlockElements"].append({"Type": "Page Break"})
   return qsf_output
 
-def buildDisplayLogic(question, cur_question, qsf_output):
-  if question[display_logic_index] == "":
+def buildSkipLogic(question, cur_question, qsf_output):
+  if question[term_index] == "":
+    return cur_question
+
+  if comma_new_line == "comma":
+    skip_logics = question[term_index].split(";")
+  else:
+    skip_logics = question[term_index].split("\n")
+
+  skip_logic_value = []
+
+  for skip_logic_iteration, skip_logic in enumerate(skip_logics):
+    cur_skip_logic = resetSkipLogic()
+
+    cur_choice_index = skip_logics[skip_logic_iteration].split()[-1].strip()
+    if "is not" in skip_logics[skip_logic_iteration].lower():
+      Condition = "NotSelected"
+    else:
+      Condition = "Selected"
+
+    cur_skip_logic["Condition"] = Condition
+    cur_skip_logic["QuestionID"] = cur_question["PrimaryAttribute"]
+    cur_skip_logic["ChoiceLocator"] = "q://" + str(cur_question["PrimaryAttribute"]) + "/SelectableChoice/" + str(cur_choice_index)
+    cur_skip_logic["Locator"] = "q://" + str(cur_question["PrimaryAttribute"]) + "/SelectableChoice/" + str(cur_choice_index)
+    cur_skip_logic["SkipToDestination"] = "ENDOFBLOCK"
+
+    global skip_logic_id
+    cur_skip_logic["SkipLogicID"] = skip_logic_id
+    skip_logic_id = skip_logic_id + 1
+
+    skip_logic_value.append(cur_skip_logic)
+
+  qsf_output["SurveyElements"][0]["Payload"][cur_block_index]["BlockElements"][-2]["SkipLogic"] = skip_logic_value
+  return cur_question
+
+def buildTerms(question, cur_question, qsf_output):
+  if question[term_index] == "":
+    return cur_question
+
+  global cur_flow_index
+  cur_flow_index = cur_flow_index + 1
+  branch_flow_id = "FL_" + str(cur_flow_index)
+  cur_flow_index = cur_flow_index + 1
+  data_flow_id = "FL_" + str(cur_flow_index)
+  cur_flow_index = cur_flow_index + 1
+  term_flow_id = "FL_" + str(cur_flow_index)
+
+  cur_term_block = {
+    "Type": "Branch",
+    "FlowID": branch_flow_id,
+    "Description": "New Branch",
+    "BranchLogic":
+    {
+        "0": {},
+        "Type": "BooleanExpression"
+    },
+    "Flow":
+    [
+      {
+        "Type": "EmbeddedData",
+        "FlowID": data_flow_id,
+        "EmbeddedData":
+        [
+          {
+            "Description": "term",
+            "Type": "Custom",
+            "Field": "term",
+            "VariableType": "String",
+            "DataVisibility":
+            [],
+            "AnalyzeText": False,
+            "Value": question[question_number_index] + " " + question[term_index].lower()
+          },
+          {
+            "Description": "gc",
+            "Type": "Custom",
+            "Field": "gc",
+            "VariableType": "String",
+            "DataVisibility":
+            [],
+            "AnalyzeText": False,
+            "Value": "2"
+          }
+        ]
+      },
+      {
+        "Type": "Standard",
+        "ID": preview_term_id,
+        "FlowID": "FL_11",
+        "Autofill":
+        []
+      }, 
+      {
+        "Type": "EndSurvey",
+        "FlowID": term_flow_id
+      }
+    ]
+  }
+
+  cur_term_block["BranchLogic"] = buildDisplayLogic(question,cur_question, qsf_output, True)
+
+  #Add branch to flow
+  qsf_output['SurveyElements'][1]["Payload"]["Flow"].insert(-2,cur_term_block)
+  flow_count = {
+    "Count":cur_flow_index
+  }
+  qsf_output['SurveyElements'][1]["Payload"]["Properties"] = flow_count
+
+  #Add skip logic
+  cur_question = buildSkipLogic(question, cur_question, qsf_output)
+
+  return cur_question
+
+def buildDisplayLogic(question, cur_question, qsf_output, from_term):
+  #This function builds display logic as well as branch logic (the from_term bool)
+  if from_term == False:
+    logic_index = display_logic_index
+  else:
+    logic_index = term_index
+
+  if question[logic_index] == "":
     return cur_question
 
   display_logic_value = {
@@ -503,32 +755,44 @@ def buildDisplayLogic(question, cur_question, qsf_output):
   }
   
   if comma_new_line == "comma":
-    display_logics = question[display_logic_index].split(",")
+    display_logics = question[logic_index].split(";")
   else:
-    display_logics = question[display_logic_index].split("\n")
+    display_logics = question[logic_index].split("\n")
 
   for display_logic_iteration, display_logic in enumerate(display_logics):
     cur_display_logic = resetDisplayLogic()
 
-    cur_export_tag = display_logics[display_logic_iteration].split("is",1)[0].strip()
+    if from_term == False:
+      cur_export_tag = display_logics[display_logic_iteration].split("is",1)[0].strip()
+    else:
+      cur_export_tag = cur_question["PrimaryAttribute"]
+
     if display_logic_iteration > 0:
-      conjunction = cur_export_tag.split()[0]
-      cur_export_tag = cur_export_tag.split(' ', 1)[1]
+      if from_term == False:
+        conjunction = cur_export_tag.split()[0]
+        cur_export_tag = cur_export_tag.split(' ', 1)[1]
+      else:
+        conjunction = display_logics[display_logic_iteration].split("is",1)[0].strip()
+
       cur_display_logic["Conjuction"] = conjunction.capitalize()
 
     cur_choice_index = display_logics[display_logic_iteration].split()[-1].strip()
-    if "is not" in display_logics[display_logic_iteration]:
+    if "is not" in display_logics[display_logic_iteration].lower():
       Operator = "NotSelected"
     else:
       Operator = "Selected"
 
     cur_display_logic["Operator"] = Operator
 
-    for question_iteration, question in enumerate(qsf_output["SurveyElements"]):
-      if "DataExportTag" in question["Payload"] and question["Payload"]["DataExportTag"] == cur_export_tag:
-        QuestionID = question["PrimaryAttribute"]
-        ChoiceLocator = "q://" + QuestionID + "/SelectableChoice/" + cur_choice_index
-        continue
+    if from_term == False:
+      for question_iteration, question in enumerate(qsf_output["SurveyElements"]):
+        if "DataExportTag" in question["Payload"] and question["Payload"]["DataExportTag"] == cur_export_tag:
+          QuestionID = question["PrimaryAttribute"]
+          ChoiceLocator = "q://" + QuestionID + "/SelectableChoice/" + cur_choice_index
+          continue
+    else:
+      QuestionID = cur_question["PrimaryAttribute"]
+      ChoiceLocator = "q://" + cur_question["PrimaryAttribute"] + "/SelectableChoice/" + cur_choice_index
 
     cur_display_logic["QuestionID"] = QuestionID
     cur_display_logic["ChoiceLocator"] = ChoiceLocator
@@ -538,12 +802,54 @@ def buildDisplayLogic(question, cur_question, qsf_output):
 
   display_logic_value["0"]["Type"] = "If"
   display_logic_value["Type"] = "BooleanExpression"
-  display_logic_value["inPage"] = False
-  cur_question["Payload"]["DisplayLogic"] = display_logic_value
-  return cur_question
+
+  if from_term == False:
+    display_logic_value["inPage"] = False
+    cur_question["Payload"]["DisplayLogic"] = display_logic_value
+    return cur_question
+  else:
+    return display_logic_value
+
+def addPreviewBlocks(qsf_output):
+  global final_question_id
+  global preview_term_id
+  global preview_variables_id
+
+  #Build preview: term question
+  cur_question_id = "QID" + str(final_question_id + 2)
+  preview_term_question["PrimaryAttribute"] = cur_question_id
+  preview_term_question["Payload"]["QuestionID"] = cur_question_id
+  qsf_output["SurveyElements"].append(preview_term_question)
+
+  #Build preview: term block
+  preview_block = resetPreviewBlock()
+  preview_block["Description"] = "Preview: Term"
+  preview_block["ID"] = preview_term_id
+  preview_block["BlockElements"][0]["QuestionID"] = cur_question_id
+  qsf_output["SurveyElements"][0]["Payload"].append(preview_block)
+
+  #Build preview: variables question
+  cur_question_id = "QID" + str(final_question_id + 3)
+  preview_variables_question["PrimaryAttribute"] = cur_question_id
+  preview_variables_question["Payload"]["QuestionID"] = cur_question_id
+  qsf_output["SurveyElements"].append(preview_variables_question)
+
+  #Build preview: variables block
+  preview_block = resetPreviewBlock()
+  preview_block["Description"] = "Preview: Variables"
+  preview_block["ID"] = preview_variables_id
+  preview_block["BlockElements"][0]["QuestionID"] = cur_question_id
+  qsf_output["SurveyElements"][0]["Payload"].append(preview_block)
+
+  return qsf_output
 
 def createQuestions(qsf_input, qsf_output):
+  #Used to know the final id to insert the preview blocks
+  global final_question_id
+
   for question_iteration, question in enumerate(qsf_input[1:]):
+    final_question_id = question_iteration
+
     if question[question_type_index].lower() != "text entry":
       cur_question = resetQuestionMultipleChoice()
     else:
@@ -581,12 +887,18 @@ def createQuestions(qsf_input, qsf_output):
     cur_question = buildMatrixTableScalePoints(question, cur_question)
 
     #Build display logic
-    cur_question = buildDisplayLogic(question, cur_question, qsf_output)
+    cur_question = buildDisplayLogic(question, cur_question, qsf_output, False)
 
     qsf_output['SurveyElements'].append(cur_question)
 
     # Add question to block
     qsf_output = addQuestionToBlock(question_iteration, qsf_output, question, qsf_input)
+
+    #Build terms
+    cur_question = buildTerms(question, cur_question, qsf_output)
+
+  #Add preview blocks
+  qsf_output = addPreviewBlocks(qsf_output)
   return qsf_output
 
 def convertCsvToQsf(qsf_output):
